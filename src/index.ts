6e1e0160e0fd561473e4a50dc16c7ad3d5250c40
Dennis de Oliveira <contato@dennisoliveira.com.br>
2024-10-24 19:00:25 -0300

    Adicionando prettier ao projeto

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ const app: Application = express()
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Products Parser API')
+  res.send('Products Parser API')
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`)
 })

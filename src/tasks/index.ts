2b7eb439b4d7b450747a542feae4e2500a24f004
Dennis de Oliveira <contato@dennisoliveira.com.br>
2024-10-28 17:38:49 -0300

    Forçando o carregametno dos dados na execução para fins de teste

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -2,6 +2,8 @@ import cron from 'node-cron'
 import openfoodfactsETLTasks from './openFoodFactsETL'
 
 const scheduleTasks = () => {
+  // Forçando rodar pelo menos na primeira vez para ter dados carregados em caso de teste
+  openfoodfactsETLTasks()
   cron.schedule('0 0 * * *', () => {
     console.log('Iniciando a tarea agendada openfoodfactsETLTasks...')
     openfoodfactsETLTasks()
